fa8413e518e9c35bcbb127af0818414d3fd3d9e6
Fabian Veltkamp <veltkamp@antony-it.de>
2022-10-31 12:31:37 +0100

    Initial commit

---
+++ fabsi.MauiExample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace fabsi.MauiExample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ fabsi.MauiExample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace fabsi.MauiExample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
